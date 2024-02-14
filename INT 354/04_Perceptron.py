0d15e5948108403eaa073301fa46db51b2f8f283
Sriram Akula <sriramakula212@gmail.com>
2024-02-14 09:28:55 +0530

    perceptron

--- INT 354/04_Perceptron.py
+++ INT 354/04_Perceptron.py
@@ -40,3 +40,28 @@ X=iris.data[:,[2,3]]
 y=iris.target
 print('Class labels:',np.unique(y))
 
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=1,stratify=y)
+
+print('Labels counts in y:',np.bincount(y))
+print('Labels counts in y-train:',np.bincount(y_train))
+print('Labels counts in y-test:',np.bincount(y_test))
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler() #Normalization means to make data in btw 0 and 1
+sc.fit(X_train) #training data fed for standardization
+X_train_std=sc.transform(X_train) #Transforming the train data strandardized
+X_test_std = sc.transform(X_test) #Transforming the test data strandardized
+
+from sklearn.linear_model import Perceptron
+ppn=Perceptron(eta0=0.1,random_state=1)
+ppn.fit(X_train_std,y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclssified examples: %d'%(y_test !=y_pred).sum())
+
+from sklearn.metrics import accuracy_score
+print("Accuracy: %.3f" % accuracy_score(y_test,y_pred))
+
+print('Accuracy: %.3f' % ppn.score(X_test_std,y_test))
+
