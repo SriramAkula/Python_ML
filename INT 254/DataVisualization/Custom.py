5cbeb9d6a52e827cec37387605d46cfa56f305cf
Sriram Akula <sriramakula212@gmail.com>
2024-02-27 00:47:52 +0530

    add

--- INT 254/DataVisualization/Custom.py
+++ INT 254/DataVisualization/Custom.py
@@ -6,4 +6,17 @@ print("----------------------")
 #Adding a dummy column of only zeroes
 df['zero']=0
 print(df)
+print("----------------------")
 print(df.__len__())
+print("----------------------")
+df.drop('dummy',axis=1,inplace=True)
+print(df)
+print("----------------------")
+#drop the row
+# df=df.drop(2)
+# print(df)
+print("----------------------")
+df.drop(2,inplace=True)
+print(df)
+df.drop(2,axis=1,inplace=True)
+print(df)
