44eddc32f0eb1062ae17c02f90a0cfd746f37579
Sriram Akula <sriramakula212@gmail.com>
2024-09-13 19:27:21 +0530

    ses2

---
+++ INDUSTRIALINTENSHIP/Comp_Ops.py
@@ -0,0 +1,12 @@
+''' 
+    ==  <=
+    !=  >=
+    >   <
+    Assignment #Num=10
+'''
+num=int(input("Enter Num: "))
+
+if(num%2==0):
+    print("Even")
+else:
+    print("Odd")
---
+++ INDUSTRIALINTENSHIP/Ses2_1.py
@@ -0,0 +1,6 @@
+age = 35
+salary = 60000
+if age>=30 and salary >= 50000:
+    print("Eligible for the Premium membership")
+else:
+    print("Not Eligible for the membership")
